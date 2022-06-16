b87a74f97b7c57f82360795ef661f653d5a1c901
jefntungila <jef_95@ymail.com>
2022-06-16 21:16:27 +0200

    worked on tabbar for a bit

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,55 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(150),
-          child: AppBar(
-            flexibleSpace: SafeArea(
-              child: Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/jubi_logo.png'),
-                        fit: BoxFit.cover)),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(150),
+            child: AppBar(
+              flexibleSpace: SafeArea(
+                child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/jubi_logo.png'),
+                          fit: BoxFit.cover)),
+                ),
+              ),
+              bottom: TabBar(
+                labelColor: Colors.black,
+                tabs: [
+                  Tab(
+                      icon: Icon(
+                        Icons.home,
+                        color: Colors.black,
+                      ),
+                      text: 'home'),
+                  Tab(
+                    icon: Icon(
+                      Icons.home,
+                      color: Colors.black,
+                    ),
+                    text: 'thoughts',
+                  ),
+                  Tab(
+                    icon: Icon(
+                      Icons.home,
+                      color: Colors.black,
+                    ),
+                    text: 'writing',
+                  ),
+                  Tab(
+                    icon: Icon(
+                      Icons.home,
+                      color: Colors.black,
+                    ),
+                    text: 'life',
+                  ),
+                ],
               ),
             ),
           ),
-        ),
-        body: Container());
+          body: Container()),
+    );
   }
 }
